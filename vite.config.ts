280cbed78d4fa7f626aed7b8ebf009e6f87400f0
Ayush Namdeo <ayush.namdeo440@gmail.com>
2025-01-03 16:11:40 +0530

    outDir Build to Dist

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'build',
+    outDir: 'dist',
     assetsInlineLimit: 0,
   },
   plugins: [
